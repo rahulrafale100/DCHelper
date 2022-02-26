a378433ce2833b27007c93d807c53842704c290b
sreyansh192 <sreyanshjain192@gmail.com>
2022-02-26 12:35:43 +0530

    hii all

--- app/src/main/java/com/example/dchelper/MainActivity.java
+++ app/src/main/java/com/example/dchelper/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0;i<10;i++){
             System.out.println("Fuck u");
         }
-        while(true){
+        for(true){
             System.out.println("Hello Wolrd");
         }
     }
